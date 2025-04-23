7141f836558f8410fc949e4eb56d1bd76b5ce6b4
Arafat <arafat.developer2008@gmail.com>
2025-04-23 14:30:16 +0600

    parsenInt

--- index.js
+++ index.js
@@ -2,3 +2,9 @@
 var age = 20;
 console.log(age);
 console.log(typeof age);
+
+var status = true;
+console.log(typeof status);
+const num1 = 30.5;
+const num2 = 15.8;
+console.log(parseInt(num1+num2));
